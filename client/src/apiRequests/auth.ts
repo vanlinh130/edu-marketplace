c97c2b03223885b0fcf0d7750297f10389ad7b33
vanlinh130 <linhvanle130@gmail.com>
2025-06-16 15:23:32 +0700

    ui auth google

---
+++ client/src/apiRequests/auth.ts
@@ -0,0 +1,37 @@
+import CommonConstants from "@/constants/common";
+import http from "@/lib/https";
+
+interface User {
+    id: string;
+    name: string;
+    email: string;
+}
+
+const authApiRequest = {
+    login: async (credential: string) => {
+        const res = await http.post<{
+            accessToken: string;
+            expiresAt: string;
+            user: User;
+        }>(CommonConstants.API_APP_LOGIN_PATH, { credential });
+
+        const { accessToken, expiresAt, user } = res;
+
+        await fetch('/api/auth/token', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include', 
+            body: JSON.stringify({
+                accessToken,
+                expiresAt,
+                userInfo: user,
+            }),
+        });
+
+        return res;
+    },
+
+    logout: async () => http.post(CommonConstants.API_APP_LOGOUT_PATH, {}),
+}
+
+export default authApiRequest;
--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import LoginForm from "./login-form";
+import LoginGoogle from "./login-google";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="w-[320px] py-[5%] m-auto" id="login">
+      <div className="flex flex-col">
+        <div className="flex items-center justify-center">
+          <Link href="/" className="">
+            <Image
+              src="/images/logo/logo02.png"
+              alt=""
+              width={206}
+              height={74}
+              className="py-2.5 object-contain"
+            />
+          </Link>
+        </div>
+        <div className="mt-[35px] mb-[10px]">
+          <h2 className="text-[#1E2132] text-[20px] font-bold">Đăng Nhập</h2>
+        </div>
+        <LoginForm/>
+        <LoginGoogle/>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
